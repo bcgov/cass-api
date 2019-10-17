15b6e9cf48caa1820bfff51f996aa978717334b7
Wade Barnes <wade.barnes@shaw.ca>
2019-10-17 10:52:28 -0700

    Update settings

--- openshift/settings.sh
+++ openshift/settings.sh
@@ -4,3 +4,6 @@ export GIT_REF="cass"
 
 # Skip Jenkins pipeline processing for this project.  The pipelines are integrated into the build templates.
 export SKIP_PIPELINE_PROCESSING=1
+
+# The list of templates to ignore
+export ignore_templates="api-postgress-deploy-bluegreen"
